8359f1cdc704caff96a080c31919f8c580300af8
samsab1995 <samirsabiee@gmail.com>
2021-09-03 11:14:26 +0430

    create ArticleResource.php for transform article response data

---
+++ app/Http/Resources/ArticleResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArticleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request): array
+    {
+        return [
+            'title' => $this->title,
+            'description' => $this->description,
+            'author' => $this->user_id,
+            'image' => $this->image
+        ];
+    }
+}
