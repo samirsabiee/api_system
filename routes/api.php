c14c5e93fb5908f44fb81b306a312e49e6b8913e
samsab1995 <samirsabiee@gmail.com>
2021-09-03 13:11:54 +0430

    auth routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\AuthController;
 
 Route::group(['prefix' => 'v1'], function () {
     Route::apiResource('articles', ArticleController::class);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
 });
